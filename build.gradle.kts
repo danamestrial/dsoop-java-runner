558eae0cc6ec53bab582f6a6196017243555fd52
danamestrial <chayapolbun@gmail.com>
2024-02-06 01:56:57 +0700

    feat: changed jvm version to match real runner

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.jvmTarget = "17"
 //    kotlinOptions.suppressWarnings = true
 //    kotlinOptions.verbose = false
 }
